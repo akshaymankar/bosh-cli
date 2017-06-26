a10839f9f54b50ac6e2f6af89475efd1b49c3d3c
Akshay Mankar <amankar@pivotal.io>
2017-06-26 17:55:53 +0100

    Remove unnecessary code from fakes

--- ui/fakes/fake_stage.go
+++ ui/fakes/fake_stage.go
@@ -1,9 +1,5 @@
 package fakes
 
-import (
-	biui "github.com/akshaymankar/int-yaml/ui"
-)
-
 type FakeStage struct {
 	PerformCalls []*PerformCall
 	SubStages    []*FakeStage
@@ -18,56 +14,4 @@ type PerformCall struct {
 
 func NewFakeStage() *FakeStage {
 	return &FakeStage{}
-}
-
-func (s *FakeStage) Perform(name string, closure func() error) error {
-
-	call := &PerformCall{Name: name}
-
-	// lazily instantiate to make matching sub-stages easier
-	if s.PerformCalls == nil {
-		s.PerformCalls = []*PerformCall{}
-	}
-	s.PerformCalls = append(s.PerformCalls, call) //We want to record the calls in the same order as the real implementation would print them
-
-	err := closure()
-
-	call.Error = err
-	if err != nil {
-		if skipErr, isSkipError := err.(biui.SkipStageError); isSkipError {
-			call.SkipError = skipErr
-			err = nil
-		}
-	}
-
-	return err
-}
-
-func (s *FakeStage) PerformComplex(name string, closure func(biui.Stage) error) error {
-	subStage := NewFakeStage()
-
-	// lazily instantiate to make matching simple stages easier
-	if s.SubStages == nil {
-		s.SubStages = []*FakeStage{}
-	}
-	s.SubStages = append(s.SubStages, subStage)
-
-	err := closure(subStage)
-
-	call := &PerformCall{Name: name, Error: err, Stage: subStage}
-
-	if err != nil {
-		if skipErr, isSkipError := err.(biui.SkipStageError); isSkipError {
-			call.SkipError = skipErr
-			err = nil
-		}
-	}
-
-	// lazily instantiate to make matching sub-stages easier
-	if s.PerformCalls == nil {
-		s.PerformCalls = []*PerformCall{}
-	}
-	s.PerformCalls = append(s.PerformCalls, call)
-
-	return err
 }
